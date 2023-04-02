63ac4ae42f2bd69069c3ea3e642c49763a26a0a0
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-04-01 21:49:38 -0600

    01-03-2023 - Splash screen
    Se agrega animacion lottie para splash screen

--- MyDailyNote/app/src/main/java/com/davidglez/mydailynote/ui/splash/MainActivity.kt
+++ MyDailyNote/app/src/main/java/com/davidglez/mydailynote/ui/splash/MainActivity.kt
@@ -8,8 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.davidglez.mydailynote.R
 import com.davidglez.mydailynote.ui.theme.MyDailyNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyDailyNoteTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                    color = MaterialTheme.colorScheme.background) {
+                    LottieAnimationView()
                 }
             }
         }
     }
+
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LottieAnimationView() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.my_daily_note_splash_animation))
+    val progress by animateLottieCompositionAsState(composition)
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyDailyNoteTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
